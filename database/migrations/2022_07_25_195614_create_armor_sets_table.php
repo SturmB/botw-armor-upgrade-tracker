3b170ad28c3a687e2fc611876a015e674a177e07
Chris McGee <chris@chrismcgee.info>
2022-07-27 17:13:17 -0400

    🎨 Prettifies the two WIP migrations

--- database/migrations/2022_07_25_195614_create_armor_sets_table.php
+++ database/migrations/2022_07_25_195614_create_armor_sets_table.php
@@ -4,19 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('armor_sets', function (Blueprint $table) {
+        Schema::create("armor_sets", function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
-            $table->string('image', 100);
+            $table->string("name", 100);
+            $table->string("image", 100);
             $table->timestamps();
         });
     }
@@ -26,8 +25,8 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('armor_sets');
+        Schema::dropIfExists("armor_sets");
     }
 };
--- database/migrations/2022_07_25_195659_create_armors_table.php
+++ database/migrations/2022_07_25_195659_create_armors_table.php
@@ -4,21 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('armors', function (Blueprint $table) {
+        Schema::create("armors", function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
-            $table->string('image', 100);
-            $table->boolean('upgradable');
-            $table->foreignId('armor_sets')
+            $table->string("name", 100);
+            $table->string("image", 100);
+            $table->boolean("upgradable");
+            $table
+                ->foreignId("armor_sets")
                 ->nullable()
                 ->constrained()
                 ->cascadeOnUpdate()
@@ -32,8 +32,8 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('armors');
+        Schema::dropIfExists("armors");
     }
 };
