cca49bd171a776078da266f72c2f5f007b245dd2
Chris McGee <chris@chrismcgee.info>
2022-08-06 17:43:41 -0400

    🗃️ Make some columns nullable

--- database/migrations/2022_07_25_195659_create_armors_table.php
+++ database/migrations/2022_07_25_195659_create_armors_table.php
@@ -15,9 +15,9 @@ return new class extends Migration {
         Schema::create("armors", function (Blueprint $table) {
             $table->smallIncrements("id");
             $table->string("name", 100);
-            $table->string("image", 100);
+            $table->string("image", 100)->nullable();
             $table->boolean("upgradable")->default(1);
-            $table->unsignedSmallInteger("armor_set_id");
+            $table->unsignedSmallInteger("armor_set_id")->nullable();
             $table->timestamps();
 
             $table
