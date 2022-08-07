0222ab487af9e6085b6347bf64bdad3c30530867
Chris McGee <chris@chrismcgee.info>
2022-08-07 09:17:46 -0400

    🗃️ Makes one more column nullable

--- database/migrations/2022_08_02_013222_create_resources_table.php
+++ database/migrations/2022_08_02_013222_create_resources_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::create("resources", function (Blueprint $table) {
             $table->smallIncrements("id");
             $table->string("name", 100);
-            $table->string("image", 100);
+            $table->string("image", 100)->nullable();
             $table->timestamps();
         });
     }
