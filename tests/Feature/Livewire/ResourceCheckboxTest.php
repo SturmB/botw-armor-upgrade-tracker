c7918ad5177b9d96f7fde0dbbb446918056a29d9
Chris McGee <chris@chrismcgee.info>
2022-08-28 22:56:35 -0400

    ✅ Adds tests for ResourceCheckbox component

---
+++ tests/Feature/Livewire/ResourceCheckboxTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\Livewire;
+
+use App\Http\Livewire\ResourceCheckbox;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class ResourceCheckboxTest extends TestCase
+{
+    /** @test */
+    public function the_resource_checkbox_can_render()
+    {
+        $component = Livewire::test(ResourceCheckbox::class);
+
+        $component->assertStatus(200);
+    }
+
+    /** @test */
+    public function passed_data_is_set_correctly()
+    {
+        $passedData = [
+            "resourceName" => "Bokoblin Horn",
+            "quantity" => 5,
+            "pivotId" => 1,
+        ];
+
+        Livewire::test(ResourceCheckbox::class, $passedData)
+            ->assertSet("resourceName", "Bokoblin Horn")
+            ->assertSee("Bokoblin Horn")
+            ->assertSet("quantity", 5)
+            ->assertSee(5)
+            ->assertSet("pivotId", 1)
+            ->assertSee(1);
+    }
+
+    /** @test */
+    public function clicking_checkbox_emits_event()
+    {
+        Livewire::test(ResourceCheckbox::class, ["pivotId" => 1])
+            ->set("checked", true)
+            ->call("checkboxClicked")
+            ->assertEmitted("checkboxClicked");
+    }
+}
