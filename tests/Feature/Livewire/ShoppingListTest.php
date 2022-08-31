76b32b63891a58f822b807ab758a285748ba0fab
Chris McGee <chris@chrismcgee.info>
2022-08-31 17:13:00 -0400

    🧪 Adds test for combining resource quantities upon checkbox click

--- tests/Feature/Livewire/ShoppingListTest.php
+++ tests/Feature/Livewire/ShoppingListTest.php
@@ -6,6 +6,7 @@ use App\Http\Livewire\ShoppingList;
 use App\Models\Armor;
 use App\Models\Requirement;
 use App\Models\Resource;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -40,5 +41,30 @@ class ShoppingListTest extends TestCase
         Livewire::test(ShoppingList::class)
             ->emit("checkboxClicked", true, $requirement->id)
             ->assertSee("{$requirement->quantity_needed} of {$requirement->resource->name}");
+    }
+
+    /** @test */
+    public function shopping_list_shows_combined_quantities()
+    {
+        $requirementOne = Requirement::factory()
+            ->for(Armor::factory()->create())
+            ->for(Resource::factory()->create([
+                "name" => "Amber"
+            ]))
+            ->create();
+        $requirements = Requirement::factory(2)
+            ->for(Armor::factory()->create())
+            ->for(Resource::factory()->create([
+                "name" => "Bokoblin Horn"
+            ]))
+            ->create();
+        $requirements->prepend($requirementOne);
+
+        $totalQuantity = $requirements->where("resource.name", "Bokoblin Horn")->pluck("quantity_needed")->sum();
+
+        Livewire::test(ShoppingList::class)
+            ->emit("checkboxClicked", true, $requirements[1]->id)
+            ->emit("checkboxClicked", true, $requirements[2]->id)
+            ->assertSee("{$totalQuantity} of {$requirements[1]->resource->name}");
     }
 }
