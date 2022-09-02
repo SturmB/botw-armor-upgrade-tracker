f6902ac06a2748a4454e5d895184b9a6fe95d1e4
Chris McGee <chris@chrismcgee.info>
2022-09-02 17:37:23 -0400

    🛂 Login/logout functionality working

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,27 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Armor Upgrade Tracker</title>
+
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     @livewireStyles
 </head>
 <body>
+
+@if (Route::has('login'))
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        @auth
+            <livewire:navigation-menu />
+        @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+        @endauth
+    </div>
+@endif
 
 <h1>Armor Sets</h1>
 
