11a147cfb08f0616ec4a5a98d705e5a4b123170e
Chris McGee <chris@chrismcgee.info>
2023-04-26 17:08:34 -0400

    💄 Fixes checkboxes

--- resources/views/livewire/armor-card.blade.php
+++ resources/views/livewire/armor-card.blade.php
@@ -1,6 +1,6 @@
 <li class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg text-center shadow @if($isActive) bg-white @else bg-gray-200 @endif">
     @if($armor->upgradable)
-        <x-jet-checkbox class="absolute m-6 h-12 w-12 text-botw-blue-500 focus:border-botw-blue-300 focus:ring-botw-blue-200" wire:model="isActive" wire:change="onChange" />
+        <input type="checkbox" class="absolute m-6 h-12 w-12 rounded border-gray-300 text-botw-blue-500 shadow-sm focus:border-botw-blue-300 focus:ring focus:ring-botw-blue-200 focus:ring-opacity-50" wire:model="isActive" wire:change="onChange" />
     @endif
     <div class="flex flex-1 flex-col p-8">
         <img class="mx-auto h-32 w-32 flex-shrink-0 @if(!$isActive) grayscale opacity-50 @endif" src="{{ asset("storage/images/{$armor->image}") }}" alt="">
