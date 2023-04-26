afb3282cc2a0aaf7e7312a4a2346476bf1c5729a
Chris McGee <chris@chrismcgee.info>
2023-04-26 11:25:13 -0400

    💄 Styles the empty shopping list text

--- resources/views/livewire/shopping-list.blade.php
+++ resources/views/livewire/shopping-list.blade.php
@@ -7,7 +7,10 @@
                     :resourceId="$item['resourceId']"
                 />
             @empty
-                <p>Empty Shopping List</p>
+                <div class="flex justify-center items-center gap-3 font-semibold text-lg text-gray-400">
+                    <i class="fa-sharp fa-regular fa-clipboard-list fa-lg"></i>
+                    <h3>Empty Shopping List</h3>
+                </div>
             @endforelse
         </ul>
     @endif
