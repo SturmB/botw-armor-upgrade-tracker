184f29f41d99d20f17a11ac76dab1d5a480654b5
Chris McGee <chris@chrismcgee.info>
2023-05-01 14:54:10 -0400

    🌱 Removes fake test users from seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,17 +9,15 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        User::factory(10)->create();
+        // User::factory(10)->create();
 
-        User::factory()->create([
-            "name" => "Test User",
-            "email" => "test@example.com",
-        ]);
+        // User::factory()->create([
+        //     "name" => "Test User",
+        //     "email" => "test@example.com",
+        // ]);
 
         $this->call([
             ArmorSetSeeder::class,
