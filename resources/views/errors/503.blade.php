5cdc1a7f88f51a425659ae129d7620132fcbe997
Chris McGee <chris@chrismcgee.info>
2023-07-04 12:55:03 -0400

    💄 Updates maintenance mode screen

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,15 @@
+<x-guest-layout>
+    <x-authentication-card>
+        <x-slot name="logo">
+            <x-authentication-card-logo />
+        </x-slot>
+
+        <div class="flex flex-col items-center justify-center max-w-2xl">
+            <i class="fa-light fa-triangle-exclamation fa-5x mb-5"></i>
+            <h1 class="mb-4 text-3xl font-bold text-center uppercase">We’ll be back soon</h1>
+            <p class="text-center">
+                Sorry for the inconvenience. We're performing some maintenance at the moment and will be back shortly.
+            </p>
+        </div>
+    </x-authentication-card>
+</x-guest-layout>
